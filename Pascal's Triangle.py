4688cd98aa7d5b379455f2b3b7b8862cd69905fb
Gopal Katariya <katariyagopal60@gmail.com>
2024-01-18 20:35:27 +0530

    pascals_triangle

--- Pascal's Triangle.py
+++ Pascal's Triangle.py
@@ -1,21 +1,14 @@
 def pascals_triangle(n):
-    ans = []
+    ans = [[1]]
     for i in range(1, n):
-        if ans:
-            temp = [1]
-            for j in range(1, len(ans[-1])):
-                temp.append(ans[-1][j - 1] + ans[-1][j])
-            temp.append(1)
-            ans.append(temp)
-        else:
-            pass
-            temp = []
-            for k in range(i):
-                temp.append(1)
-            ans.append(temp)
+        temp = [1]
+        for j in range(1, len(ans[-1])):
+            temp.append(ans[-1][j - 1] + ans[-1][j])
+        temp.append(1)
+        ans.append(temp)
     print(ans)
 
 
 if __name__ == '__main__':
-    n = 7
+    n = 5
     pascals_triangle(n)
