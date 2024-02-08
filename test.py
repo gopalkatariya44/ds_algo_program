f890994a0f8194329e9256afa20952af26bce220
Gopal Katariya <katariyagopal60@gmail.com>
2024-02-08 22:11:26 +0530

    test

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
+class Parent():
+    def __call__(self, *args, **kwargs):
+        print("hey")
+
+
+class Test(Parent):
+    def test1(self):
+        print("test1")
+
+    def test2(self):
+        print("test2")
+
+    def test3(self):
+        print("test3")
+
+
 if __name__ == '__main__':
-    pass
+    test = Test()
+    test()
