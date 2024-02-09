86ae375ae20c0c94d5a61fe5b5ee1cc572daf816
Gopal Katariya <katariyagopal60@gmail.com>
2024-02-10 00:16:05 +0530

    multiply

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        n1, n2 = 0, 0
+
+        for num in num1:
+            print(n1 * 10, ord(num) - 48)
+            n1 = n1 * 10 + ord(num) - 48
+        print(n1)
+
+        for num in num2:
+            n2 = n2 * 10 + ord(num) - 48
+
+        return f"{n1 * n2}"
+
 class Parent():
     def __call__(self, *args, **kwargs):
         print("hey")
@@ -15,5 +29,15 @@ class Test(Parent):
 
 
 if __name__ == '__main__':
-    test = Test()
-    test()
+    # test = Test()
+    # test()
+    s = Solution()
+
+    print(s.multiply("22", "33"))
+    n1 = 0
+    for i in "123456789":
+        print(n1*10)
+        print(n1*10 + ord(i)-48)
+        n1 = n1*10 + ord(i)-48
+    print(n1)
+
