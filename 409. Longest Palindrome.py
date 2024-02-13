b4cde2c096554d5301eb43e70e1f2a1ae4cef400
Gopal Katariya <katariyagopal60@gmail.com>
2024-02-13 18:21:06 +0530

    longest Palindrome

--- 409. Longest Palindrome.py
+++ 409. Longest Palindrome.py
@@ -27,3 +27,30 @@ def longestPalindrome(s: str) -> int:
 if __name__ == '__main__':
     s = "awdfesgdhjhjngbfvdc"
     print(longestPalindrome(s))
+
+count = dict()
+for i in s:
+    count[i] = count.get(i, 0) + 1
+
+ones = 0
+ans = 0
+ls = []
+odd = False
+
+for i in count.keys():
+    if count[i] == 1:
+        ls.append(1)
+    elif count[i] % 2 == 0:
+        ans += count[i]
+    elif count[i] % 2 != 0 and count[i] > 1:
+        if odd == False:
+            ans += count[i]
+            ones = 1
+            odd = True
+        else:
+            ans += (count[i] - 1)
+
+if len(ls) > 0 and ones == 0:
+    ans += 1
+
+print(ans)
